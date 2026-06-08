88001c7c8551c220554257e548a0585ca2ee6648
nicolas <ni.fernandezj@duocuc.cl>
2026-06-08 19:05:51 -0400

    tester

--- ticketgol/compras/src/main/java/ticketgol/compras/Model/Compras.java
+++ ticketgol/compras/src/main/java/ticketgol/compras/Model/Compras.java
@@ -1,49 +1,52 @@
 package ticketgol.compras.Model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Date;
+
 @Entity
 @Table(name = "compras")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Compras {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Long id;
+    private Long id;
 
+    @NotBlank(message = "El ID del usuario es obligatorio no puede estar vacío")
+    @Size(max = 100, message = "El ID no puede tener más de 100 caracteres")
+    @Column(name = "usuario_id", nullable = false, length = 100)
+    private String usuarioId;
 
-    @NotBlank (message = "El ID del usuario es obligatorio no puede estar vacio")
-    @Size (max = 100 ,message = "EL ID no puede tener mas de 100 caracteres")
-    @Column(name = "userId",nullable = false,length = 100)
-    private  String usuarioId;
-
-    @NotBlank (message = "El ID del ticket es obligatorio no puede estar vacio")
-    @Size (max = 100,message = "El ID de ticket no puede tener mas de 100 caracteres")
-    @Column (name = "ticketId",nullable = false,length = 100)
+    @NotBlank(message = "El ID del ticket es obligatorio no puede estar vacío")
+    @Size(max = 100, message = "El ID de ticket no puede tener más de 100 caracteres")
+    @Column(name = "ticket_id", nullable = false, length = 100)
     private String ticketId;
 
-    @NotBlank(message = "El codigo QR es obligatorio ")
-    @Size(max = 100,message = "El codigo QR no puede tener mas de 100 caracteres ")
-    @Column(name = "qrCode")
+    @NotBlank(message = "El código QR es obligatorio")
+    @Size(max = 100, message = "El código QR no puede tener más de 100 caracteres")
+    @Column(name = "codigo_qr", nullable = false, length = 100)
     private String codigoQr;
+
+    @NotNull(message = "La fecha de compra es obligatoria")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fecha_compra", nullable = false)
+    private Date fechaCompra;
 
     @NotBlank(message = "El método de pago es obligatorio.")
     @Size(max = 100, message = "El método de pago no puede tener más de 100 caracteres.")
-    @Column(name = "metodoPago", nullable = false, length = 100)
+    @Column(name = "metodo_pago", nullable = false, length = 100)
     private String metodoPago;
 
-    @NotNull(message = "El estado de la compra es obligatorio.")
+    @NotBlank(message = "El estado de la compra es obligatorio.")
     @Size(max = 100, message = "El estado no puede tener más de 100 caracteres.")
-    @Column(name = "status", nullable = false, length = 100)
-    private Boolean estado;
-
-
-
+    @Column(name = "estado", nullable = false, length = 100)
+    private String estado;
 }
