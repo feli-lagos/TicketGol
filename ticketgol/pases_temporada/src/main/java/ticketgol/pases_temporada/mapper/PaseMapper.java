151c3271c33ee4b1f7ddac4284f426aefe5cfd8f
MartinS-Gith <martin.simon.canales@gmail.com>
2026-06-10 01:49:55 -0400

    a

--- ticketgol/pases_temporada/src/main/java/ticketgol/pases_temporada/mapper/PaseMapper.java
+++ ticketgol/pases_temporada/src/main/java/ticketgol/pases_temporada/mapper/PaseMapper.java
@@ -1,33 +1,12 @@
 package ticketgol.pases_temporada.mapper;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import ticketgol.pases_temporada.model.PaseTemporada;
 import ticketgol.pases_temporada.model.PaseTemporadaDtoFront;
 
-@Component
-public class PaseMapper {
-
-    public void updatePassFromDto(PaseTemporadaDtoFront dto, PaseTemporada entity) {
-        if (dto == null || entity == null) {
-            return;
-        }
-
-        if (dto.getTemporada() != null) {
-            entity.setTemporada(dto.getTemporada());
-        }
-
-        if (dto.getFecha_final() != null) {
-            entity.setFecha_final(dto.getFecha_final());
-        }
-
-        if (dto.getClubId() != null) {
-            entity.setClubId(dto.getClubId());
-        }
-
-        if (dto.getSeccionId() != null) {
-            entity.setSeccionId(dto.getSeccionId());
-        }
-
-        entity.setEstado(dto.isEstado());
-    }
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface PaseMapper {
+    void updatePassFromDto(PaseTemporadaDtoFront dto, @MappingTarget PaseTemporada entity);
 }
