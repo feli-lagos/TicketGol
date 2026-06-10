9982e6d4b54637498d25f429bb28e702cae87978
FelipeGrin4 <falbenites@gmail.com>
2026-06-09 22:27:47 -0400

    Se suben los cambios de Martín porque se buggeó GIT

--- ticketgol/pases_temporada_M/src/main/java/ticketgol/pases_temporada/model/PaseTemporada.java
+++ ticketgol/pases_temporada_M/src/main/java/ticketgol/pases_temporada/model/PaseTemporada.java
@@ -1,5 +1,6 @@
 package ticketgol.pases_temporada.model;
 
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,8 +8,8 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.time.LocalDate;
+
+import java.sql.Date;
 
 @Data
 @NoArgsConstructor
@@ -26,32 +27,28 @@ public class PaseTemporada {
     @NotBlank(message = "la temporada no puede estar en blanco")
     private String temporada;
 
-    @JsonProperty("fecha_inicio")
-    @Column(name = "fecha_inicio", nullable = false)
+    @Column(name = "fecha_inicio", length = 50, nullable = false)
     @NotNull(message = "la fecha no puede estar vacía")
-    private LocalDate fecha_inicio;
+    private Date fecha_inicio;
 
-    @JsonProperty("fecha_final")
-    @Column(name = "fecha_final", nullable = false)
+    @Column(name = "fecha_final", length = 50, nullable = false)
     @NotNull(message = "el pase de temporada debe tener fecha de término")
-    private LocalDate fecha_final;
+    private Date fecha_final;
 
-    @Column(name = "estado", nullable = false)
+    @Column(name = "estado", length = 50, nullable = false)
     @NotNull(message = "el estado debe estar vigente o no")
     private boolean estado;
 
-    @JsonProperty("userRut")
-    @Column(name = "user_rut", nullable = false)
-    @NotBlank(message = "el RUT del usuario no puede estar en blanco")
-    private String userRut;
+    @Column(name = "user_id", nullable = false)
+    @NotNull(message = "el id no puede estar en blanco")
+    private Long userId;
 
-    @JsonProperty("clubId")
     @Column(name = "club_id", nullable = false)
     @NotNull(message = "el id no puede estar en blanco")
     private Long clubId;
 
-    @JsonProperty("seccionId")
     @Column(name = "seccion_id", nullable = false)
     @NotNull(message = "el id no puede estar en blanco")
     private Long seccionId;
+
 }
--- ticketgol/pases_temporada_M/src/main/java/ticketgol/pases_temporada/webclient/PaseUsuario.java
+++ ticketgol/pases_temporada_M/src/main/java/ticketgol/pases_temporada/webclient/PaseUsuario.java
@@ -1,25 +1,47 @@
 package ticketgol.pases_temporada.webclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import ticketgol.pases_temporada.mapper.UsuarioMapper;
+import ticketgol.pases_temporada.model.UsuarioEstadoDto;
+
 import java.util.Map;
 
 @Component
 public class PaseUsuario {
 
-    private final WebClient webClient;
+    @Autowired
+    private UsuarioMapper usuarioMapper;
 
-    public PaseUsuario(@Value("${usuario.service.url}") String clienteServidor) {
+    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
+
+
+    public PaseUsuario(@Value("${usuario-service.url}") String clienteServidor, WebClient.Builder webClientBuilder, UsuarioMapper usuarioMapper){
         this.webClient = WebClient.builder().baseUrl(clienteServidor).build();
+        this.webClientBuilder = webClientBuilder;
+        this.usuarioMapper = usuarioMapper;
     }
 
-    public Mono<Map<String, Object>> getUsuarioByRut(String rut) {
+    public Map<String, Object> getUsuarioById(Long id){
         return this.webClient.get()
-                .uri("/rut/{rut}", rut)
+                .uri("/{id}", id)
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<>() {});
+                .onStatus(httpStatusCode -> httpStatusCode.is4xxClientError(),
+                clientResponse -> clientResponse.bodyToMono(String.class)
+                .map(body -> new RuntimeException("cliente no encontrado"))).bodyToMono(Map.class).block();
+    }
+
+    public UsuarioEstadoDto getUsuarioDtoById(Long id){
+        Map<String, Object> respuestaUsuario = webClientBuilder.build().get()
+                .uri("/{id}", id)
+                .retrieve()
+                .onStatus(httpStatusCode -> httpStatusCode.is4xxClientError(),
+                        clientResponse -> clientResponse.bodyToMono(String.class)
+                                .map(body -> new RuntimeException("cliente no encontrado"))).bodyToMono(Map.class)
+                .block();
+        return usuarioMapper.toDto(respuestaUsuario);
     }
 }
